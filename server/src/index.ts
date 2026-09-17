168786192373d02c4b7607a90281d4ce73ee6cf4
AYUSHAMAN MISHRA <149884884+ayushmanmishra18@users.noreply.github.com>
2026-09-18 04:36:35 +0530

    fix server TypeScript null and ObjectId errors

--- server/src/index.ts
+++ server/src/index.ts
@@ -39,7 +39,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static('uploads'));
 
 app.use((req, res, next) => {
-  req.io = io;
+  (req as any).io = io;
   next();
 });
 
