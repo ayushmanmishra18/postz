94985cfbd03b235b6b17279fd70c166ed5b6edce
AYUSHAMAN MISHRA <149884884+ayushmanmishra18@users.noreply.github.com>
2026-09-18 04:13:45 +0530

    register recovery routes

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -17,5 +17,7 @@ export default function AuthLayout() {
   return <Stack screenOptions={{ headerShown: false, animation: 'fade' }}>
     <Stack.Screen name="login" />
     <Stack.Screen name="signup" />
+    <Stack.Screen name="forgot-password" />
+    <Stack.Screen name="reset-password" />
   </Stack>;
 };
