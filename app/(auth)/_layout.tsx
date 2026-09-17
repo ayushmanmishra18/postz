976d7b6740df5031bf71f0a5ec14d034a12035ba
AYUSHAMAN MISHRA <149884884+ayushmanmishra18@users.noreply.github.com>
2026-09-18 03:52:22 +0530

    Fix auth routes to match existing screens

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -6,8 +6,6 @@ export default function AuthLayout() {
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="login" />
       <Stack.Screen name="signup" />
-      <Stack.Screen name="forgot-password" />
-      <Stack.Screen name="reset-password" />
     </Stack>
   );
 }
